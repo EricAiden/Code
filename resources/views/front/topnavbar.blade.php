d5eb09e6f8033002fb5baab6e3ecaf9852d0d729
hacktran <01216603581abc@gmial.com>
2022-11-05 16:07:30 +0700

    topnavbar

--- resources/views/front/topnavbar.blade.php
+++ resources/views/front/topnavbar.blade.php
@@ -6,10 +6,23 @@
         <span class="navbar-toggler-icon"></span>
     </button> --}}
     <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-        <div class="navbar-nav py-0" style="width: 850px; justify-content: space-between">
-            <a href="index.html" class="nav-item nav-link active">Laptop</a>
-            <a href="shop.html" class="nav-item nav-link">Tablet</a>
-            <a href="detail.html" class="nav-item nav-link">PC-linh kiện</a>
+        <ul class="navbar-nav py-0" style="width: 850px; justify-content: space-between">
+            <li class="nav-item nav-link nav-item-hover">
+                <a href="#" class="link-laptop">
+                    <div class="icon-laptop">
+                        <svg class="svg-laptop" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M128 32C92.7 32 64 60.7 64 96V352h64V96H512V352h64V96c0-35.3-28.7-64-64-64H128zM19.2 384C8.6 384 0 392.6 0 403.2C0 445.6 34.4 480 76.8 480H563.2c42.4 0 76.8-34.4 76.8-76.8c0-10.6-8.6-19.2-19.2-19.2H19.2z"/></svg>
+                    </div>
+                    Laptop
+                </a>
+                <div class="wrap-laptop">
+                    <div class="laptop-items laptop-products"></div>
+                    <div class="laptop-items laptop-price"></div>
+                    <div class="laptop-items laptop-img"></div>
+                </div>
+            </li>
+
+            <li href="shop.html" class="nav-item nav-link">Tablet</li>
+            <li href="detail.html" class="nav-item nav-link">PC-linh kiện</li>
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Phụ kiện</a>
                 <div class="dropdown-menu rounded-0 m-0">
@@ -17,7 +30,7 @@
                     <a href="checkout.html" class="dropdown-item">Checkout</a>
                 </div>
             </div>
-        </div>
+        </ul>
         <div class="navbar-nav ml-auto py-0">
             <a href="" class="nav-item nav-link">Login</a>
             <a href="" class="nav-item nav-link">Register</a>
